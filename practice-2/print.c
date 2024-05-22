ec1b2929b2789645b11acecbce5c11a4756a2634
JoySarkarBD <developer.joysarkar@gmail.com>
2024-05-22 11:12:57 +0600

    practice 2

---
+++ practice-2/print.c
@@ -0,0 +1,18 @@
+# include <stdio.h> // Header file / library file
+
+int main(){
+  
+  printf("Return something!\n"); // printf is a built-in function
+  printf("Here is the second line!\n"); 
+
+  printf("//\n"); 
+  printf("%%"); 
+  
+  return 0;
+}
+
+// Special characters (ESCAPE)
+// \n - new line, like enter button action in ms-word, txt, docx
+// \t - tab, x spaces     
+
+  
